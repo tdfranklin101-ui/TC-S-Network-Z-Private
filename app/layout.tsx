4a38f5d7549d2d83f0ab354c5dd91438cadbfda8
Terry Franklin <41223061-tdfranklin101@users.noreply.replit.com>
2025-12-02 04:36:31 +0000

    fix: Clean layout.tsx and remove src/ conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,27 @@
 import TCSFooter from "@/components/tcs/TCSFooter";
 import TCSTopNav from "@/components/tcs/TCSTopNav";
 import TCSSolarBackground from "@/components/tcs/TCSSolarBackground";
+import "./globals.css";
+
 export const metadata = {
-  title: "TC-S Application",
+  title: "TC-S Z Private",
   description: "Part of the TC-S Network Constellation"
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-      <TCSSolarBackground>
-        <div className="min-h-screen flex flex-col">
-          <TCSTopNav />
-          <div className="flex-1">
-
-      <TCSSolarBackground>
-        <div className="min-h-screen flex flex-col">
-          <TCSTopNav />
-          <div className="flex-1">
-{children}
+        <TCSSolarBackground>
+          <div className="min-h-screen flex flex-col">
+            <TCSTopNav />
+            <div className="flex-1">
+              {children}
+            </div>
+            <TCSFooter />
           </div>
-          <TCSFooter />
-        </div>
-      </TCSSolarBackground>
-    
-          </div>
-          <TCSFooter />
-        </div>
-      </TCSSolarBackground>
-    </body>
+        </TCSSolarBackground>
+      </body>
     </html>
   );
 }
